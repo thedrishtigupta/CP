8c7cfd5a1c5d207dc5c8d3bd00bc884e3f51bdb3
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-21 00:31:41 +0530

    CSES: Weird algorithm

--- CSES/Weird_algo.cpp
+++ CSES/Weird_algo.cpp
@@ -1,9 +1,20 @@
-
-
 #include <iostream>
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    return 0;
+    long long n; cin >> n;
+
+    while(1) {
+        cout << n;
+        if (n == 1) break;
+        cout << ' ';
+
+        if (n & 1) n = 3 * n + 1;
+        else n >>= 1;
+    }
+
+    cout << endl;
 }
