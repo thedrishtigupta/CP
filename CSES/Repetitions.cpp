f1bf13d5b24d0e96c97404afc77940cc8cdcb71c
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-21 20:08:05 +0530

    CSES: Repetitions, longest substring

---
+++ CSES/Repetitions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include<climits>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s; cin>>s;
+    long long len = 1;
+    long long ans = 1;
+
+    for(int i = 1; i < s.length(); i++) {
+        if (s[i] == s[i-1]) len++;
+        else len = 1;
+        ans = max(ans, len);
+    }
+
+    cout<<ans<<endl;
+}
