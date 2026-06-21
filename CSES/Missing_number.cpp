d8f8108452b44a2d9125c9c12229f7b28c47ea0d
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-21 20:01:44 +0530

    CSES: Missing Number

---
+++ CSES/Missing_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n; cin >> n;
+    long long sum = 0;
+    for(auto i = 1; i < n; i++) {
+        int x; cin>>x;
+        sum += x;
+    }
+
+    long long expSum = n * (n+1) / 2;
+
+    cout << expSum - sum<<endl;
+}
