ec2073a15f65c1ed54e08b371c33b26ba86534ad
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-24 23:49:57 +0530

    CSES : Permutations

---
+++ CSES/6_Permutations.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n; cin >> n;
+    
+    if (n == 2 || n == 3) {cout<<"NO SOLUTION"<<endl; return 0;}
+
+    for(int i = 2; i <= n; i+=2) cout<<i<<" ";
+    for(int i = 1; i <= n; i+=2) cout<<i<<" ";
+
+    cout<<endl;
+
+    return 0;
+}
